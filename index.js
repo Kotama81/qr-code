704a474bca52131adbcbf7114fd3a8198cb00c79
Kotama81 <kotama1981@gmail.com>
2024-06-03 09:41:33 +0200

    Create a qr_img.png from answers.url

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ inquirer
   ])
   .then((answers) => {
     const url = answers.url
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
     
   })
   .catch((error) => {
