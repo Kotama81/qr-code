f176dd70183e1f4ff4d4aca70ef2a0a9153d0416
Kotama81 <kotama1981@gmail.com>
2024-06-03 09:14:27 +0200

    Input question from users and saved answers to a variable

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ import inquirer from 'inquirer';
 import qr from 'qr-image';
 import fs from 'fs';
 
+inquirer
+  .prompt([
+    {type: "input",
+    name: "url",
+    message: "Type url!",
+    }
+  ])
+  .then((answers) => {
+    const url = answers.url
+    
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
